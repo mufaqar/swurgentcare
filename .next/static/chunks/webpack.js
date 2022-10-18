2e2dbad82a846debc8915f36e1b0034308809ca2
Mufaqar Islam <mufaqar@gmail.com>
2022-10-18 11:54:13 +0500

    updated

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -150,7 +150,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7095f424ec68d48d"; }
+/******/ 		__webpack_require__.h = function() { return "0ee0011350283848"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
