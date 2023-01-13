51c7df66d749e7b826b50fdd0a22b208c2d85c97
syed-zunair-gillani <gillanix007@gmail.com>
2023-01-13 20:02:14 +0500

    up

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -31,12 +31,12 @@ const Slug = ({ post, all_services }) => {
         <section className="services-area section-gap">
           <div className="container">
             <div className="row justify-content-center">
-              <div className="col-lg-8 order-lg-last">
+              <div className="col-lg-12 order-lg-last">
                 <div className="service-details-wrapper">
                   <figure className="mt-b relative">
                     <OwnImage path={post.featuredImage?.node.mediaItemUrl} />
                   </figure>
-                  <div className="content_wrapper">
+                  <div className="content_wrapper blog_single">
                     <div
                       className=""
                       dangerouslySetInnerHTML={{
@@ -46,81 +46,7 @@ const Slug = ({ post, all_services }) => {
                   </div>
                 </div>
               </div>
-              <div className="col-lg-4 col-md-10 order-lg-first">
-                <div className="service-sidebar">
-                  <div className="widget departments-list">
-                    <h3 className="widget-title">Departments</h3>
-                    <ul>
-                      {all_services.map((service, i) => (
-                        <li key={i}>
-                          <a
-                            className="cap"
-                            onClick={() => router.push(`/${service?.slug}`)}
-                          >
-                            <i className="far fa-angle-right" />
-                            {service.title}
-                          </a>
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-
-                  <div className="widget working-schedule">
-                    <h3 className="widget-title">Working Hour</h3>
-                    <ul>
-                      <li>
-                        <span>
-                          <i className="far fa-angle-right" />
-                          Monday
-                        </span>
-                        <span>09.00 – 08.00</span>
-                      </li>
-                      <li>
-                        <span>
-                          <i className="far fa-angle-right" />
-                          Tuesday
-                        </span>
-                        <span>09.00 – 08.00</span>
-                      </li>
-                      <li>
-                        <span>
-                          <i className="far fa-angle-right" />
-                          Wednesday
-                        </span>
-                        <span>09.00 – 08.00</span>
-                      </li>
-                      <li>
-                        <span>
-                          <i className="far fa-angle-right" />
-                          Thursday
-                        </span>
-                        <span>09.00 – 08.00</span>
-                      </li>
-                      <li>
-                        <span>
-                          <i className="far fa-angle-right" />
-                          Friday
-                        </span>
-                        <span>09.00 – 08.00</span>
-                      </li>
-                      <li>
-                        <span>
-                          <i className="far fa-angle-right" />
-                          Saturday
-                        </span>
-                        <span>09.00 – 03.00</span>
-                      </li>
-                      <li>
-                        <span>
-                          <i className="far fa-angle-right" />
-                          Sunday
-                        </span>
-                        <span>09.00 – 01.00</span>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
+              
             </div>
           </div>
         </section>
