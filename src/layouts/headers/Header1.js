9251985aaba451ace7838cc4b33994a245402588
mufaqar <mufaqar@gmail.com>
2023-05-11 17:05:49 +0500

    Header

--- src/layouts/headers/Header1.js
+++ src/layouts/headers/Header1.js
@@ -1,62 +1,67 @@
-import Link from "next/link";
-import React, { useState } from "react";
-import OwnImage from "../../components/OwnImage";
-import {Pages} from "./Menu";
-import MobileMenu from "./MobileMenu";
-import SlidePanel from "./SlidePanel";
+import Link from 'next/link';
+import React, { useState } from 'react';
+import OwnImage from '../../components/OwnImage';
+import { Pages } from './Menu';
+import MobileMenu from './MobileMenu';
+import SlidePanel from './SlidePanel';
 
 const Header1 = ({ headerTopbar, position }) => {
   const [sidebarTrigger, setSidebarTrigger] = useState(false);
   const [mobileTrigger, setMobileTrigger] = useState(false);
 
-  const origin = typeof window !== 'undefined' && window.location.origin ? window.location.origin : '';
+  const origin =
+    typeof window !== 'undefined' && window.location.origin
+      ? window.location.origin
+      : '';
   const URL = `${origin}`;
 
   return (
     <header
-      className={`template-header sticky-header header-one ${position ? position + "-header" : "" }`}
+      className={`template-header sticky-header header-one ${
+        position ? position + '-header' : ''
+      }`}
     >
       {headerTopbar && (
         <>
-        <div className="header-topbar d-none d-md-block">
-          <div className="container-fluid container-1400">
-            <div className="row align-items-center justify-content-between">
-              <div className="col-lg-6">
-                <div className="topbar-menu">
-                  <span style={{ listStyle: 'none' }} >
-                    400 Promenade Wy Ste 1500, Sugar Land, TX 77478, <span className="last">United States</span>
-                  </span>
+          <div className="header-topbar d-none d-md-block">
+            <div className="container-fluid container-1400">
+              <div className="row align-items-center justify-content-between">
+                <div className="col-lg-6">
+                  <div className="topbar-menu">
+                    <span style={{ listStyle: 'none' }}>
+                      400 Promenade Wy Ste 1500, Sugar Land, TX 77478,{' '}
+                      <span className="last">United States</span>
+                    </span>
+                  </div>
                 </div>
-              </div>
-              <div className="col-lg-6">
-                <ul className="contact-info">
-                 
-                  <li>
-                    <a href="#">
-                      <i className="far fa-phone" /> Call US : (346) 341-7573
-                    </a>
-                  </li>
-                  <li>
-                    <div className="social-icons">
+                <div className="col-lg-6">
+                  <ul className="contact-info">
+                    <li>
                       <a href="#">
-                        <i className="fab fa-facebook-f" />
+                        <i className="far fa-phone" /> Call US : (346) 341-7573
                       </a>
-                      <a href="#">
-                        <i className="fab fa-twitter" />
-                      </a>
-                      <a href="#">
-                        <i className="fab fa-youtube" />
-                      </a>
-                      <a href="#">
-                        <i className="fab fa-linkedin-in" />
-                      </a>
-                    </div>
-                  </li>
-                </ul>
+                    </li>
+                    <li>
+                      <div className="social-icons">
+                        <a href="#">
+                          <i className="fab fa-facebook-f" />
+                        </a>
+                        <a href="#">
+                          <i className="fab fa-twitter" />
+                        </a>
+                        <a href="#">
+                          <i className="fab fa-youtube" />
+                        </a>
+                        <a href="#">
+                          <i className="fab fa-linkedin-in" />
+                        </a>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
-        </div>
         </>
       )}
 
@@ -68,7 +73,6 @@ const Header1 = ({ headerTopbar, position }) => {
                 <OwnImage path="/assets/site-logo.png" alt="swurgentcare" />
               </Link>
             </div>
-
           </div>
           <div className="header-right">
             <ul className="extra-icons">
@@ -93,29 +97,14 @@ const Header1 = ({ headerTopbar, position }) => {
                     </ul>
                   </li>
                   <li>
-                    <a href={`${URL}/#services`}>
-                      Services
-                    </a>
+                    <a href={`${URL}/#services`}>Services</a>
                   </li>
                   <li>
                     <Link href={`${URL}/blog`}>
                       <a>Blogs</a>
                     </Link>
                   </li>
-                  <li>
-                    <Link href="/doctor">
-                      <a>
-                        Providers
-                      </a>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="health-vault">
-                      <a>
-                        Health Vault
-                      </a>
-                    </Link>
-                  </li>
+
                   <li>
                     <Link href="/contact">Contact Us</Link>
                   </li>
