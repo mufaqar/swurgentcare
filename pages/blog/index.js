92685b9aa167af4e11377fafacc99a3b927fde7b
mufaqar <mufaqar@gmail.com>
2023-05-16 12:24:25 +0500

    updated sitemap

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,28 +1,32 @@
-import { gql } from "@apollo/client";
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
-import { client } from "../../lib/client";
-import { getAllBlogs, GetAllServices } from "../../lib/queries";
-import PageBanner from "../../src/components/PageBanner";
-import Layouts from "../../src/layouts/Layouts";
+import { gql } from '@apollo/client';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import { client } from '../../lib/client';
+import { getAllBlogs, GetAllServices } from '../../lib/queries';
+import PageBanner from '../../src/components/PageBanner';
+import Layouts from '../../src/layouts/Layouts';
 
 const Blog = ({ all_services, blogs }) => {
   return (
     <Layouts footer={2} services={all_services}>
-      <PageBanner title={"Blogs"} />
+      <PageBanner title={'Blogs'} />
       <section className="blog_posts container mx-auto mt-5 row gx-5">
-      {
-        blogs.map((blog, i)=>{
-          return(
+        {blogs.map((blog, i) => {
+          return (
             <div key={i} className="_blog col-md-6 col-lg-4">
-              <Image src={blog.node.featuredImage?.node.mediaItemUrl} alt={blog.node.title} width={500} height={500}/>
-              <Link href={`/blog/${blog.node.slug}`}><h4 className="">{blog.node.title}</h4></Link>
+              <Image
+                src={blog.node.featuredImage?.node.mediaItemUrl}
+                alt={blog.node.title}
+                width={500}
+                height={500}
+              />
+              <Link href={`/blog/${blog.node.slug}`} passHref>
+                <h4 className="">{blog.node.title}</h4>
+              </Link>
             </div>
-          )
-        })
-      }
-        
+          );
+        })}
       </section>
     </Layouts>
   );
@@ -31,8 +35,12 @@ const Blog = ({ all_services, blogs }) => {
 export default Blog;
 
 export async function getServerSideProps() {
-  const GET_POSTS = gql`${getAllBlogs}`;
-  const GET_SERVICES = gql`${GetAllServices}`;
+  const GET_POSTS = gql`
+    ${getAllBlogs}
+  `;
+  const GET_SERVICES = gql`
+    ${GetAllServices}
+  `;
 
   const response = await client.query({
     query: GET_POSTS,
@@ -48,7 +56,7 @@ export async function getServerSideProps() {
   return {
     props: {
       blogs,
-      all_services
+      all_services,
     },
   };
 }
--- src/components/signature.js
+++ src/components/signature.js
@@ -1,43 +1,52 @@
-import React, { Component } from 'react'
-import SignaturePad from 'react-signature-canvas'
+import React, { Component } from 'react';
+import SignaturePad from 'react-signature-canvas';
 
-import styles from '../../styles/Home.module.css'
+import styles from '../../styles/Home.module.css';
 
-export default class Signature extends Component  {
-  state = {trimmedDataURL: null}
+export default class Signature extends Component {
+  state = { trimmedDataURL: null };
 
-  sigPad = {}
+  sigPad = {};
   clear = () => {
-    this.sigPad.clear()
-  }
+    this.sigPad.clear();
+  };
   trim = () => {
-    this.setState({trimmedDataURL: this.sigPad.getTrimmedCanvas()
-      .toDataURL('image/png')})
-  }
-  render () {
-    let {trimmedDataURL} = this.state
+    this.setState({
+      trimmedDataURL: this.sigPad.getTrimmedCanvas().toDataURL('image/png'),
+    });
+  };
+  render() {
+    let { trimmedDataURL } = this.state;
 
     // console.log("🚀 ~ file: signature.js ~ line 18 ~ signature ~ render ~ trimmedDataURL", trimmedDataURL)
-    return <div className={styles.container}>
-      <div className={styles.sigContainer}>
-        <SignaturePad canvasProps={{className: styles.sigPad}} penColor='black' backgroundColor="white"
-          ref={(ref) => { this.sigPad = ref }} />
+    return (
+      <div className={styles.container}>
+        <div className={styles.sigContainer}>
+          <SignaturePad
+            canvasProps={{ className: styles.sigPad }}
+            penColor="black"
+            backgroundColor="white"
+            ref={(ref) => {
+              this.sigPad = ref;
+            }}
+          />
+        </div>
+        <div>
+          <button className={styles.buttons} onClick={this.clear}>
+            Clear
+          </button>
+          <button className={styles.buttons} onClick={this.trim}>
+            Add Signature
+          </button>
+        </div>
+        {trimmedDataURL ? (
+          <img
+            className={styles.sigImage}
+            src={trimmedDataURL}
+            alt="Signature"
+          />
+        ) : null}
       </div>
-      <div>
-        <button className={styles.buttons} onClick={this.clear}>
-          Clear
-        </button>
-        <button className={styles.buttons} onClick={this.trim}>
-          Add Signature
-        </button>
-      </div>
-      {trimmedDataURL
-        ? <img className={styles.sigImage}
-          src={trimmedDataURL} />
-        : null}
-    </div>
+    );
   }
 }
-
-
-
